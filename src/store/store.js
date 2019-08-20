057bdd103bd8a31433335ae044ff6a6637342014
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-08-20 08:36:55 +0300

    Add redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -2,5 +2,10 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers/index';
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState);
+  return createStore(
+    rootReducer,
+    initialState,
+    // eslint-disable-next-line no-underscore-dangle
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  );
 }
