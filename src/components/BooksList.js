80619d61f4fd463e984e66c0462bec15e54fafa1
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-08-20 16:03:41 +0300

    Refactor BookList fuction

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,6 +1,8 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { removeBook } from '../actions';
 import Book from './Book';
 import '../App.css';
 
@@ -9,29 +11,34 @@ const mapStateToProps = (state) => ({
   books: state.books,
 });
 
-function BooksList({ books }) {
-  return (
-    <div>
-      <h1>Books list</h1>
-      <table className="book-table">
-        <tbody>
-          {books.map((book) => (
-            <Book
-              key={book.id}
-              id={book.id}
-              category={book.category}
-              title={book.title}
-            />
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-}
+const BooksList = ({ books, deleteBook }) => (
+  <div>
+    <h1>Books list</h1>
+    <table className="book-table">
+      <tbody>
+        {books.map((book) => (
+          <Book
+            key={book.id}
+            id={book.id}
+            title={book.title}
+            category={book.category}
+            removeBook={deleteBook}
+          />
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
 
 BooksList.propTypes = {
   books: PropTypes.arrayOf(PropTypes.object).isRequired,
+  deleteBook: PropTypes.func.isRequired,
 };
 
 
-export default connect(mapStateToProps, null)(BooksList);
+export default connect(
+  mapStateToProps,
+  {
+    deleteBook: removeBook,
+  },
+)(BooksList);
