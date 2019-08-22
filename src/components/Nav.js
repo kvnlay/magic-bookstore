0f51903d8e8232587c218b22e9f23b4094123561
kvnlay <kingstevens007@gmail.com>
2019-08-22 17:46:53 +0100

    Add nav presentational component

---
+++ src/components/Nav.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import './App.css';
+
+const Nav = () => (
+  <div className="panel">
+    <div className="menu">
+      <h1 className="logo">
+          Bookstore CMS
+      </h1>
+      <p className="panel-book">
+          Books
+      </p>
+      <p className="panel-cat">
+          Categories
+      </p>
+    </div>
+    <div className="panel-icon">
+      <FontAwesomeIcon icon={faUser} className="profile-icon" />
+    </div>
+  </div>
+);
+
+export default Nav;
