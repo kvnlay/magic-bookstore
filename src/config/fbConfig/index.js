8fe6289cf2ab6df86c280d0ae7c64c14bc8ec564
kvnlay <kingstevens007@gmail.com>
2019-09-15 21:37:44 +0100

    Add firebase config

---
+++ src/config/fbConfig/index.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';`     `
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAv6gyNd_-x5_gJI6881j-biLr4RyOjFgg",
+  authDomain: "bookstore-cms-5ed79.firebaseapp.com",
+  databaseURL: "https://bookstore-cms-5ed79.firebaseio.com",
+  projectId: "bookstore-cms-5ed79",
+  storageBucket: "",
+  messagingSenderId: "161123647171",
+  appId: "1:161123647171:web:6d52f109981bb26809ac76"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
