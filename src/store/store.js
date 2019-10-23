c1c213811a4603d90aecdc5ee0399b4eed3b14c2
kvnlay <kingstevens007@gmail.com>
2019-10-24 00:20:15 +0100

    Add state enhancers

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,18 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import thunk from "redux-thunk";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from '../config/fbConfig'
 
-const configureStore = () => createStore(rootReducer, applyMiddleware(thunk));
+const configureStore = () =>
+  createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+      reduxFirestore(fbConfig),
+      reactReduxFirebase(fbConfig)
+    )
+  );
 
 export default configureStore;
