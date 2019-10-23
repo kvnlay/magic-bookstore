8cdb7419382737477ac736b67a0b2631b52e6251
kvnlay <kingstevens007@gmail.com>
2019-10-23 17:42:18 +0100

    Add firestore config

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,5 +1,6 @@
-import firebase from 'firebase/app';
-
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 var firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -7,10 +8,11 @@ var firebaseConfig = {
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
 firebase.analytics();
 
 export default firebase;
