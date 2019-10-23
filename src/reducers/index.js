39869d571cc75bc09b78d56bd19d53187a85bcfe
kvnlay <kingstevens007@gmail.com>
2019-10-23 17:40:54 +0100

    Add auth reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import books from './books';
 import filter from './filter';
+import auth from './auth';
 
 export default combineReducers({
   books,
-  filter
+  filter,
+  auth
 });
