5af141d7a167489c623d247fa002ba14483b449e
kvnlay <kingstevens007@gmail.com>
2019-10-23 14:26:43 +0100

    add sign in form

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 
 export default function SignIn() {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-  return (
-  	<div>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  	</div>
-  )
+  const handleSubmit = () => {};
+
+  return (
+    <div className="container">
+      <form onSubmit={() => handleSubmit} className="signinForm">
+        <div className="inputField">
+          <label htmlFor="email"></label>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="inputField">
+          <label htmlFor="password"></label>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  );
 }
