61e7c0a9a8edfb20c2a9e43eca6b57cced521501
kvnlay <kingstevens007@gmail.com>
2019-10-23 17:39:07 +0100

    Add state enhancers to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,7 @@
-import { createStore } from 'redux';
-import rootReducer from '../reducers/index';
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "../reducers/index";
+import thunk from "redux-thunk";
 
-const configureStore = () =>
-  createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+const configureStore = () => createStore(rootReducer, applyMiddleware(thunk));
 
 export default configureStore;
