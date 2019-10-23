f367bdcf5ec1179105d56f3955464a638e6b8a40
kvnlay <kingstevens007@gmail.com>
2019-10-24 00:19:42 +0100

    handle async function in action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,27 @@
-import { CREATE_BOOK, REMOVE_BOOK, CHANGE_FILTER } from './actionTypes';
+import { CREATE_BOOK, REMOVE_BOOK, CHANGE_FILTER, CREATE_BOOK_ERROR } from "./actionTypes";
+import { getProgress } from "../utilities";
 
-const createBook = book => ({
-  type: CREATE_BOOK,
-  book
-});
+const createBook = book => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firestore = getFirestore();
+    firestore.collection("books").add({
+      ...book,
+      author: "unknown",
+      category: "Action",
+      progress: getProgress()
+    }).then(() => {
+      dispatch({
+        type: CREATE_BOOK,
+        book
+      });
+    }).catch((err) => {
+      dispatch({
+        type: CREATE_BOOK_ERROR,
+        err
+      })
+    })
+  };
+};
 
 const removeBook = id => ({
   type: REMOVE_BOOK,
